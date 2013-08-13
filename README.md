730f811aef5e6f50ec4fc87876d0c95ddbdbd197
Steve Anderson <steve@fuzzwork.co.uk>
2013-08-13 22:39:01 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+These two tables are required.
+
+
+CREATE TABLE `mysales` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `regionid` int(11) DEFAULT NULL,
+  `stationid` int(11) DEFAULT NULL,
+  `systemid` int(11) DEFAULT NULL,
+  `typeid` int(11) DEFAULT NULL,
+  `bid` int(11) DEFAULT NULL,
+  `price` decimal(11,3) DEFAULT NULL,
+  `minvolume` bigint(20) DEFAULT NULL,
+  `volremain` bigint(20) DEFAULT NULL,
+  `volenter` bigint(20) DEFAULT NULL,
+  `issued` datetime DEFAULT NULL,
+  `duration` int(11) DEFAULT NULL,
+  `rang` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `mysaleskey` (`stationid`,`typeid`,`bid`,`volenter`,`issued`,`duration`,`rang`)
+);
+
+CREATE TABLE `orderhistory` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `typeid` int(11) DEFAULT NULL,
+  `regionid` int(11) DEFAULT NULL,
+  `historydate` date DEFAULT NULL,
+  `orders` int(11) DEFAULT NULL,
+  `quantity` bigint(20) DEFAULT NULL,
+  `low` decimal(10,2) DEFAULT NULL,
+  `high` decimal(10,2) DEFAULT NULL,
+  `average` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `typeid` (`typeid`,`regionid`,`historydate`)
+);
+
+
+
+update db.inc.php to have the correct connection details for your database, then point evemon at upload.php
---
+++ upload.php
@@ -0,0 +1,36 @@
+<?
+require_once('db.inc.php');
+#$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$sql='insert into evesupport.mysales (stationid,regionid,systemid,typeid,bid,price,minvolume,volremain,volenter,issued,duration,rang) values (:stationid,:regionid,:systemid,:typeid,:bid,:price,:minvolume,:volremain,:volenter,str_to_date(:issued,"%Y-%m-%dT%H:%i:%s"),:duration,:rang) on duplicate key update volremain=:volremain,issued=str_to_date(:issued,"%Y-%m-%dT%H:%i:%s")';
+$histsql="insert ignore into evesupport.orderhistory (typeid,regionid,historydate,orders,quantity,low,high,average) values (:typeid,:regionid,:date,:orders,:quantity,:low,:high,:average)";
+$stmt = $dbh->prepare($sql);
+$histstmt = $dbh->prepare($histsql);
+
+
+$parsed=json_decode($_POST['data'],true);
+
+if ($parsed['resultType']=='orders')
+{
+    $typeid=$parsed['rowsets'][0]['typeID'];
+    $regionid=$parsed['rowsets'][0]['regionID'];
+    foreach ($parsed['rowsets'][0]['rows'] as $order)
+    {
+            list($issued,$temp)=explode("+",$order[7]);
+            $bid=0;
+            if($order[6]){
+                $bid=1;
+            }
+            $stmt->execute(array(':regionid'=>$regionid,':typeid'=>$typeid,':minvolume'=>$order[5],':stationid'=>$order[9],':volenter'=>$order[4],':rang'=>$order[2],':issued'=>$issued,':price'=>$order[0],':duration'=>$order[8],':bid'=>$bid,':volremain'=>$order[1],':systemid'=>$order[10]));
+    }
+}
+else
+{
+    $typeid=$parsed['rowsets'][0]['typeID'];
+    $regionid=$parsed['rowsets'][0]['regionID'];
+    foreach ($parsed['rowsets'][0]['rows'] as $order)
+    {
+            list($date,$temp)=explode("T",$order[0]);
+            $histstmt->execute(array(':typeid'=>$typeid,':regionid'=>$regionid,':date'=>$date,':orders'=>$order[1],':quantity'=>$order[2],':low'=>$order[3],':high'=>$order[4],':average'=>$order[5]));
+    }
+}
+?>1
