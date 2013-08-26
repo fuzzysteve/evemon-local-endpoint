7550935fd6c2e02aeb0e344de9c2faef16899c64
Arthur Moore <Arthur.Moore.git@cd-net.net>
2013-08-26 01:42:23 -0400

    Modified the code to work using sqlite3.
    Changes:
    README.md is now more friendly to novice users.
    Database format now uses unix epoch time.
    Database now takes care of order uniqueness and updating.
    SQL statements updated to account for changes.
    PHP now converts time to unix epoch for the database.
    
    To test:
    Make sure 'orderhistory' works correctly.

--- upload.php
+++ upload.php
@@ -1,13 +1,19 @@
 <?php
+ini_set("log_errors", 1);
+ini_set("error_log", "php-error.log");
 require_once('db.inc.php');
 #$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$sql='insert into evesupport.mysales (stationid,regionid,systemid,typeid,bid,price,minvolume,volremain,volenter,issued,duration,rang) values (:stationid,:regionid,:systemid,:typeid,:bid,:price,:minvolume,:volremain,:volenter,str_to_date(:issued,"%Y-%m-%dT%H:%i:%s"),:duration,:rang) on duplicate key update volremain=:volremain,issued=str_to_date(:issued,"%Y-%m-%dT%H:%i:%s")';
-$histsql="insert ignore into evesupport.orderhistory (typeid,regionid,historydate,orders,quantity,low,high,average) values (:typeid,:regionid,:date,:orders,:quantity,:low,:high,:average)";
+#EVE time is UTC, and that's what EVE Mon sends us. (strtotime doesn't need this, but it stops errors)
+date_default_timezone_set('UTC');
+$sql='insert into mysales (stationid,regionid,systemid,typeid,bid,price,minvolume,volremain,volenter,issued,duration,rang) values (:stationid,:regionid,:systemid,:typeid,:bid,:price,:minvolume,:volremain,:volenter,:issued,:duration,:rang)';
+$histsql="insert ignore into orderhistory (typeid,regionid,historydate,orders,quantity,low,high,average) values (:typeid,:regionid,:date,:orders,:quantity,:low,:high,:average)";
 $stmt = $dbh->prepare($sql);
+#trigger_error(print_r($stmt, true));
 $histstmt = $dbh->prepare($histsql);
 
 
 $parsed=json_decode($_POST['data'],true);
+#trigger_error(print_r($parsed, true));
 
 if ($parsed['resultType']=='orders')
 {
@@ -15,7 +21,10 @@ if ($parsed['resultType']=='orders')
     $regionid=$parsed['rowsets'][0]['regionID'];
     foreach ($parsed['rowsets'][0]['rows'] as $order)
     {
-            list($issued,$temp)=explode("+",$order[7]);
+			#Convert given date/time to unix epoch format
+			#To go back to the way it was, use this:
+			#	strftime("%Y-%m-%dT%H:%M:%S")
+			$issued=strtotime($order[7]);
             $bid=0;
             if($order[6]){
                 $bid=1;
@@ -29,7 +38,7 @@ else
     $regionid=$parsed['rowsets'][0]['regionID'];
     foreach ($parsed['rowsets'][0]['rows'] as $order)
     {
-            list($date,$temp)=explode("T",$order[0]);
+			$date=strtotime($order[0]);
             $histstmt->execute(array(':typeid'=>$typeid,':regionid'=>$regionid,':date'=>$date,':orders'=>$order[1],':quantity'=>$order[2],':low'=>$order[3],':high'=>$order[4],':average'=>$order[5]));
     }
 }
